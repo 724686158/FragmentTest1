1d7e78d6aef7ab74fa74e8a56ef16a461a30d360
mengzicheng <724686158@qq.com>
2016-09-11 13:05:41 +0800

    动态添加碎片

--- app/src/main/java/com/example/admin/fragmenttest/MainActivity.java
+++ app/src/main/java/com/example/admin/fragmenttest/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.admin.fragmenttest;
 
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.button:
+                AnotherRightFragment fragment = new AnotherRightFragment();
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.right_fragment, fragment);
+                transaction.commit();
+                break;
+            default:
+                break;
+        }
     }
 }
